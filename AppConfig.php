cfddb8b2c131d30478d14ff02d656c073df991fb
ruaibeite <45942830+ruaibeite@users.noreply.github.com>
2024-10-12 16:40:05 +0800

    Update AppConfig.php

--- AppConfig.php
+++ AppConfig.php
@@ -3,8 +3,8 @@
  * 常量配置
  */
  class AppConfig{
-	const APPID = '00312358';
-	const CUSID = '660452053992BWE';
+	const APPID = '';
+	const CUSID = '';
     const APIURL = "https://vsp.allinpay.com/apiweb/unitorder";//生产环境
 
     const APIVERSION = '11';
